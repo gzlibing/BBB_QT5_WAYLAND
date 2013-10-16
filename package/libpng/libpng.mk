54f8e9bf116ea528ba3888b132f5495e3c9888b7
liyaoshi <liyaoshi@gmail.com>
2013-10-16 09:29:19 +0800

    libpng update for neon support

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.4.12
-LIBPNG_SERIES = 14
-LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.bz2
+LIBPNG_VERSION = 1.6.6
+LIBPNG_SERIES = 16
+LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
 LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng${LIBPNG_SERIES}/$(LIBPNG_VERSION)
 LIBPNG_LICENSE = libpng license
 LIBPNG_LICENSE_FILES = LICENSE
@@ -14,5 +14,6 @@ LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkgconf zlib
 LIBPNG_CONFIG_SCRIPTS = libpng$(LIBPNG_SERIES)-config libpng-config
 
+LIBPNG_CONF_OPT = --enable-arm-neon=yes
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
